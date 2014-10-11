4e49361c0439fcfe302a88de5625159f33843cc8
Derek Hardison <derek@hardison.co>
2014-10-10 20:13:05 -0400

    Page update

--- Server.js
+++
@@ -1,8 +0,0 @@
-/**
- * Simple HTTP server to serve the game.
- */
-
-var connect = require( 'connect' ),
-	serveStatic = require( 'serve-static' );
-
-connect().use( serveStatic( __dirname ) ).listen( 8080 );
